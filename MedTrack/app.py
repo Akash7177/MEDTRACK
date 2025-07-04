017723c625f088a0da2c72dde4664812ef5e4940
Akash7177 <127237398+Akash7177@users.noreply.github.com>
2025-07-04 20:19:48 +0530

    Update app.py

--- MedTrack/app.py
+++ MedTrack/app.py
@@ -11,10 +11,10 @@ app = Flask(__name__)
 app.secret_key = 'temp_secret_123!@#xyz987'
 
 USE_DYNAMODB = True  # Set to True when deploying on AWS
-AWS_REGION = 'us-east-1a'
+AWS_REGION = 'us-east-1'
 
 if USE_DYNAMODB:
-    dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION)
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1)
     users_table = dynamodb.Table('Users')
     appointments_table = dynamodb.Table('Appointments')
     prescriptions_table = dynamodb.Table('Prescriptions')
